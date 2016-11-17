56fc6d9987aee504444b4998ef2779b07460e566
lahtachev <lahtachev@gmail.com>
2016-11-17 16:24:08 +0400

    fixed module version

--- install/version.php
+++ install/version.php
@@ -1,6 +1,6 @@
 <?php
 
     $arModuleVersion = array (
-        'VERSION'      => '0.0.0',
-        'VERSION_DATE' => '2016-11-15',
+        'VERSION'      => '0.0.4',
+        'VERSION_DATE' => '2016-11-17',
     );
